3eb121bdab10620156c7f4cb4e02d8390844627f
Jemila Bekele <99717579+JemilaBekele@users.noreply.github.com>
2025-03-22 12:47:58 +0300

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -13,36 +13,13 @@ const ServicesPage = () => {
   return (
     <>
       <Head>
-        <title>Our Services - Innovative Solutions</title>
+        <title>Our Services </title>
         <meta
           name="description"
-          content="Explore our wide range of innovative services tailored to meet your unique business needs."
+          content="Hope Business Group."
         />
 
-        <NextSeo
-          title="Our Services - Innovative Solutions | Skyhub Technologies"
-          description="Explore our wide range of innovative services tailored to meet your unique business needs at Skyhub Technologies."
-          openGraph={{
-            title: "Our Services - Innovative Solutions | Skyhub Technologies",
-            description:
-              "Explore our wide range of innovative services tailored to meet your unique business needs at Skyhub Technologies.",
-            url: "https://www.skyhubtechnologies.com/services",
-            type: "website",
-            images: [
-              {
-                url: "https://www.skyhubtechnologies.com/og-image.jpg",
-                width: 800,
-                height: 600,
-                alt: "Skyhub Technologies",
-              },
-            ],
-          }}
-          twitter={{
-            handle: "@SkyhubTech",
-            site: "@SkyhubTech",
-            cardType: "summary_large_image",
-          }}
-        />
+      
       </Head>
       <ServiceHero />
       <WaveDecoration />
