c97560bb250f74fda72e847da016762c4a91db9c
unknown <jemilabekele17@gmail.com>
2025-03-27 16:48:19 +0300

    Fix line endings for yarn.lock

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -58,14 +58,14 @@ export default function Footer() {
         <div>
           <h5 className="text-lg font-semibold mb-4">Follow Us</h5>
           <div className="space-y-3">
-          <div className="flex gap-4">
+    
   <a href="tel:+251-911422116" className="text-gray-300 flex items-center">
     <FaPhone className="inline-block mr-2 text-white" /> +251-911422116
   </a>
   <a href="tel:+251-911840527" className="text-gray-300 flex items-center">
     <FaPhone className="inline-block mr-2 text-white" /> +251-911840527
   </a>
-</div>
+
 
   <a href="mailto:duha@hopebusiness.org" className="text-gray-300 flex items-center">
     <FaEnvelope className="inline-block mr-2 text-white" /> duha@hopebusiness.org
